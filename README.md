285ec82a1fbd158164000bd277defb4a659a5cfe
luisfteixeira11 <luisfteixeira117@gmail.com>
2026-09-03 20:21:47 -0300

    add: read_img_label.py, that indicate the tumor local in a image with cv2

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Brain Tumor Detection with YOLOv11
-_Brain tumor detection with Computer Vision YOLOv11 model. This model identifies bouding boxes that indicate tumor locations in MRI (Magnetic Resonance Imaging)._
+_Brain tumor detection with Computer Vision YOLOv11 model. This model identifies a box that indicate tumor locations in MRI (Magnetic Resonance Imaging)._
+
+
+On the first part, i did the read_img_label.py to load the images and show that with cv2 with polygon that indicate the tumor location.
 
 ## Dataset
-The data comes ready to train, with yaml, the labels and images (with data augmentation, i think, because have rotations and zoons on it, but i can't prove that).
+The data comes ready to train, with yaml, the labels and images.
 
 The dataset live in ```data/```, (I had to put it in a gitignore), I download this dataset in [kaggle](https://www.kaggle.com/datasets/pkdarabi/medical-image-dataset-brain-tumor-detection/data).
 
@@ -12,3 +15,8 @@ data/train -> /labels or /images
 data/test -> /labels or /images
 data/valid -> /labels or /images
 ```
+In ```configs/``` you can find ```data.yaml``` 
+
+## The run code of read_img_label.py
+![print show image](docs/exemple_show_im.png)
+![print segmentation instance](docs/exemple_show_segmentation.png)
--- src/read_img_label.py
+++ src/read_img_label.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 PATH_IMAGE = r"brain-tumor-detection-yolov11\data\train\images\1_jpg.rf.eee6547c09d13001fff4a45c380115aa.jpg"
 PATH_TXT = r"brain-tumor-detection-yolov11\data\train\labels\1_jpg.rf.eee6547c09d13001fff4a45c380115aa.txt"
@@ -7,10 +8,10 @@ def load_image(path):
     """
     read a image from specified file path
     
-    Args: path(str) - the path of file
+    Args: path(str) - the path file
     Returns: None
     """
-    # a command that can read the image of path (have the parameter flags, but it's optional)
+    # a command that can read the image path (have the parameter flags, but it's optional)
     image = cv2.imread(path)
 
     if image is None:
@@ -21,7 +22,7 @@ def load_image(path):
 
     # a function that wait for a key press to close the window, if you put a number like 5000(ms) the window will keep 5 seconds open to close.
     cv2.waitKey(0)
-    #obs: the destroyWindow() exists too
+    #obs: the destroyWindow() exist too
     cv2.destroyAllWindows()
 
 def read_label(path):
@@ -29,7 +30,9 @@ def read_label(path):
     Read a label from specified file path
 
     Args: path(str) - the path of file
-    Returns: label(str) - the label of the image of path
+    Returns: 
+    - label(list of str) - the list of label of the image path
+    - a error
     """
     # the try is a secure form to realize the open, can raise a error
     try:
@@ -37,11 +40,63 @@ def read_label(path):
         with open(path, "r") as file:
             # read the first line of file
             row = file.readline()
-            # splits the row per space key and take the first, that be the label
-            label = row.split()[0]
+
+            # splits the row per space key and take it, that be the label
+            # i change that, i was taken only the class, but now, is all the line splited. 
+            label = row.split()
         return label
+    
     except Exception as error:
         return f"Error: {error}"
 
-load_image(PATH_IMAGE)
-print(read_label(PATH_TXT))
+def show_boxes(path, path_txt):
+    """
+    show a image from specified file path with your segmentation instance
+    
+    Args: path(str) - the path file
+    Returns: None
+    """
+
+    # reading the image and the row of .txt
+    image = cv2.imread(path)
+    label_numbers = read_label(path_txt)
+
+    # here is a possible exeption
+    if image is None or not label_numbers:
+        print("Error: unable to read")
+
+    class_id = label_numbers[0]
+    # the variable '_' is color channels 
+    high, weight, _ = image.shape
+
+    # to show it, you need desnormalize the points of .txt, you need to put in a numpy array, because it's the form that cv2 aceppt
+    pixel = []
+    for i in range(1, len(label_numbers), 2):
+        # transforming a string in a float
+        x_normalized = float(label_numbers[i])
+        y_normalized = float(label_numbers[i+1])
+
+        # its tranformed by int because a pixel is a int number
+        x_pixel = int(x_normalized * weight)
+        y_pixel = int(y_normalized * high)
+
+        pixel.append([x_pixel, y_pixel])
+
+    # i saw that np.int32 will be necessary
+    pixel_points_array = np.array(pixel, dtype=np.int32)
+    # the cv2 accepts only a tridimensional matrix to do the polylines
+    # -1 -> num of rows auto
+    # 1 -> extra dimension
+    # 2 -> num of columns
+    pixel_points_array = pixel_points_array.reshape((-1, 1, 2))
+
+    # i don't understand why the pixel_points have to be in a list if i already reshaped this, but ok
+    cv2.polylines(image, [pixel_points_array], isClosed=True, color=(0, 255, 0), thickness=2)
+ 
+    cv2.imshow("Segmentation Instance", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    load_image(PATH_IMAGE)
+    show_boxes(PATH_IMAGE, PATH_TXT)
